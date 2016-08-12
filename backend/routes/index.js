0ffde933b65a84b35f91d19ae541905e8460a3cf
Cory Fisher <cfisher@moredirect.com>
2016-08-12 16:21:05 -0400

    Move js application to be served entirely from express.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var Twitter = require('twitter');
+var path = require('path');
+
+router.get('/', function(req, res, next) {
+	res.sendFile(path.join(__dirname, '../views/index.html'));
+});
 
 /* GET home page. */
 router.get('/api/location/tweets', function(req, res, next) {
